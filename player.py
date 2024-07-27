794a3ab8b3d00977bb69281bfac73a5f29301414
demıir antay <demir99antay@gmail.com>
2024-07-27 14:07:43 +0300

    m

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, position):
+        self.position = position
+        self.velocity = [0, 0, 0]
+
+    def move(self, direction):
+        # Update position based on direction
+        pass
+
+    def handle_mouse_click(self):
+        # Determine block position based on player aim
+        # Add or remove block at the position
+        pass
